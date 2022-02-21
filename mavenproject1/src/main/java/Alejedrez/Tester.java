038c206fd91de4ec547f05b7fe492844258d990c
ale <ale@ale>
2022-02-21 18:26:41 +0100

    Segundo commit

--- mavenproject1/src/main/java/Alejedrez/Tester.java
+++ mavenproject1/src/main/java/Alejedrez/Tester.java
@@ -17,6 +17,7 @@ public class Tester {
     public static void main(String[] args) {
         // TODO code application logic here
          Tablero tablero= new Tablero();
+         System.out.println("");
         tablero.pintarTablero();
     }
     
