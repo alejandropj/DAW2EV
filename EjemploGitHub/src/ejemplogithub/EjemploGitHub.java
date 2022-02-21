bf1c303469fd66626564c022881d89d2aeac576b
parjimal <parjimal@192.168.56.1>
2022-02-21 09:27:50 +0100

    Segundo commit

--- EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
+++ EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
@@ -17,6 +17,7 @@ public class EjemploGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola munde");
+        System.out.println("Hola neo munde");
     }
     
 }
